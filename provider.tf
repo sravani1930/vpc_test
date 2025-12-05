6913f125e523267d5515d0d4388720cbedaa3a77
sravani192019 <sravani.jamithireddy@gmail.com>
2025-12-05 11:48:49 +0530

    vpc-test

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.25.0"
+    }
+  }
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "devopsify-remote-state"
+    key    = "vpc"
+    region = "us-east-1"
+    dynamodb_dynamodb_table = "devopsify-locking"
+  }
+}
+
+
+provider "aws" {
+  # Configuration options
+}
